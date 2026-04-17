bbb9b0907ad3a89d4dfd357127d989962dc83506
Buzz3r2 <saadiqsteyn1@gmail.com>
2026-04-17 10:30:46 +0200

    Added Logs for the songs.

--- app/src/main/java/com/example/myapplication/Songs.kt
+++ app/src/main/java/com/example/myapplication/Songs.kt
@@ -1,4 +1,32 @@
 package com.example.myapplication
 
+import android.util.Log
+
 class Songs {
+    // properties
+    var name: String = ""
+    var artist: String = ""
+    var year: Int = 0
+    var duration: String = ""
+
+    // function to play song
+    fun play(){
+        Log.v("Song", "$name is playing.")
+    }
+
+    // function to pause song
+    fun pause(){
+        Log.v("Song", "$name is paused.")
+    }
+
+    // function to stop song
+    fun stop(){
+        Log.v("Song", "$name is stopped.")
+    }
+
+    // function to favourite song
+    fun favourite() : Boolean{
+        Log.v("Song", "$name is favourited.")
+        return true
+    }
 }
